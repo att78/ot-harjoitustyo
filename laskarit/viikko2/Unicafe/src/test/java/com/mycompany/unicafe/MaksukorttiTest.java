350ba4a572fe62fd191f126e8746a33a5c05f2f8
Auli Tallberg <auli.tallberg@gmail.com>
2019-11-08 17:44:52 +0200

    maksupääte kunnossa

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,14 +7,66 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
+        
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        
+        assertEquals(1000, kortti.saldo());
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenVahentaaSaldoa() {
+        kortti.otaRahaa(400);
+        assertEquals("saldo: 6.0", kortti.toString());
+    }
+    
+     @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(2500);
+        assertEquals("saldo: 35.0", kortti.toString());
+    }
+    
+         @Test
+    public void saldoEiMuutu() {        
+        kortti.otaRahaa(10000);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    
+        
+         @Test
+    public void metodinPalautusFalse() {
+        
+        
+        assertEquals(false, kortti.otaRahaa(10000));
+    }
+    
+        
+        
+         @Test
+    public void metodinPalautusTrue() {
+        
+        
+        assertEquals(true, kortti.otaRahaa(100));
+    }
+
+    
+
+    
+    
 }
